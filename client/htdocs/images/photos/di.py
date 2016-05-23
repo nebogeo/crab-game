5309cd1a2dae96cb39f9b2b7dff35cef0ab81186
dave griffiths <dave@fo.am>
2016-05-23 13:43:28 +0100

    nested pairs hopefully

--- client/htdocs/images/photos/di.py
+++ client/htdocs/images/photos/di.py
@@ -1,24 +1,17 @@
 #!/usr/bin/env python
 import os
 
-def convert_crabs():
-    for filename in os.listdir('crabs/'):
-        ext = os.path.splitext(filename)[1]
-        if filename[0:3]!="di-" and (ext==".jpg" or ext==".png"):
-            cmd="convert crabs/"+filename+" -channel Red -fx 0 +channel crabs/di-"+filename
-            print(cmd)
-            os.system(cmd)
-
+# does crabs too...
 def convert_bg(name):
     for location in os.listdir(name):
         if location[0:3]!="di-":
             for filename in os.listdir(name+"/"+location):
                 ext = os.path.splitext(filename)[1]
                 if filename[0:3]!="di-" and (ext==".jpg" or ext==".png"):
-                    cmd="convert "+name+"/"+location+"/"+filename+" -channel Red -fx 0 +channel "+name+"/di-"+location+"/"+filename
+                    cmd="convert "+name+"/"+location+"/"+filename+" -channel red,green -fx \"(r+g)/2\" "+name+"/di-"+location+"/"+filename
                     print(cmd)
                     os.system(cmd)
 
-convert_crabs()
 convert_bg("mudflat")
 convert_bg("rockpool")
+convert_bg("musselbed")
