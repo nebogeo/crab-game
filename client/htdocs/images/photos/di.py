5a38d373e894dc7f4bf23782238634e08c0a1eb3
dave griffiths <dave@fo.am>
2016-06-15 17:31:51 +0100

    new photos

--- client/htdocs/images/photos/di.py
+++ client/htdocs/images/photos/di.py
@@ -12,6 +12,36 @@ def convert_bg(name):
                     print(cmd)
                     os.system(cmd)
 
-#convert_bg("mudflat")
+convert_bg("mudflat")
 convert_bg("rockpool")
-#convert_bg("musselbed")
+convert_bg("musselbed")
+
+def gen_lists(name):
+    print(name)
+    for location in os.listdir(name):
+        if location[0:3]!="di-" and location!="crabs":
+            for filename in os.listdir(name+"/"+location):
+                ext = os.path.splitext(filename)[1]
+                if filename[0:3]!="di-" and (ext==".jpg" or ext==".png"):
+                    cmd="\""+location+"/"+filename+"\""
+                    print(cmd)
+
+def gen_crab_lists(name):
+    for location in os.listdir(name):
+        if location=="crabs":
+            print(name)
+            for filename in os.listdir(name+"/"+location):
+                ext = os.path.splitext(filename)[1]
+                if filename[0:3]!="di-" and (ext==".jpg" or ext==".png"):
+                    cmd="\""+filename+"\""
+                    print(cmd)
+
+
+
+#gen_crab_lists("mudflat")
+#gen_crab_lists("musselbed")
+#gen_crab_lists("rockpool")
+
+#gen_lists("mudflat")
+#gen_lists("musselbed")
+#gen_lists("rockpool")
